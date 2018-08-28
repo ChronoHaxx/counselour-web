e0c491363e41c56b39543560598973e4d663002b
chronohax <mhazimiskp@gmail.com>
2018-08-29 00:32:43 +0800

    conflicts in serving static fildes to html.

--- counselour_web/settings.py
+++ counselour_web/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'counselour_web',
     'roombooking',
     'boards',
 ]
@@ -120,3 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
